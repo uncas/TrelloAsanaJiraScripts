b4f3e7142f09c4865c729d4f4b1c6bdfafed6723
Ole Lynge Sørensen <olelynge@gmail.com>
2023-10-03 20:31:47 +0200

    Add files via upload

--- FilterMyTasks.py
+++ FilterMyTasks.py
@@ -2,7 +2,7 @@ import csv
 
 def readTasks():
     result = []
-    with open('Oles_tasks_schibstedcom.csv', newline='') as csvfile:
+    with open('_Weekly_Promises_.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         for row in reader:
             result.append(row)
@@ -11,22 +11,35 @@ def readTasks():
 def filterActiveTasks(tasks):
     result = []
     for task in tasks:
-        assignee = task["Assignee"]
         completedAt = task["Completed At"]
-        if not completedAt and assignee == "Ole Lynge Soerensen":
+        if not completedAt:
             result.append(task) 
     return result
 
 def writeTasks(tasks):
-    fieldNames = ['Name', 'Due Date', 'Tags', 'Notes']
-    with open('Oles_active_tasks.csv', 'w', newline='') as csvfile:
+    fieldNamesToMap = ['Name', 'Due Date', 'Tags', 'Notes', '\ufeffTask ID', 'Parent task', 'Assignee Email', 'Section/Column']
+    fieldNames = fieldNamesToMap.copy()
+    fieldNames.append('Parent ID')
+    fieldNames.append('Task Type')
+    with open('Balance_active_tasks.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(fieldNames)
         for task in tasks:
             def mapField(fieldName):
                 return task[fieldName]
-            values = map(mapField, fieldNames)
-            writer.writerow(values)
+            values = list(map(mapField, fieldNamesToMap))
+            taskType = 'Task'
+            parentId = None
+            if task['Parent task']:
+                taskType = 'Subtask'
+                parentTask = [x for x in tasks if x['Name'] == task['Parent task']]
+                if (parentTask):
+                    parentId = parentTask[0]['\ufeffTask ID']
+            values.append(parentId)
+            values.append(taskType)
+            if taskType == 'Task':
+                writer.writerow(values)
+                print(task['Name'])
 
 def analyzeTasks(tasks):
     for task in tasks:
@@ -35,11 +48,12 @@ def analyzeTasks(tasks):
 
 tasks = readTasks()
 
-print(len(tasks))
+print("All tasks: ", len(tasks))
+#print(tasks[0])
 
 activeTasks = filterActiveTasks(tasks)
 
-print(len(activeTasks))
-print(activeTasks[0])
+print("Active tasks: ", len(activeTasks))
+#print(activeTasks[0])
 
 writeTasks(activeTasks)
