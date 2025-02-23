c067e4e5d243057a240ae5bef85a71677f8d8cd0
Ole Lynge Soerensen <ole.lynge.soerensen@schibsted.com>
2025-02-23 19:18:54 +0100

    Add stuff

--- Jira2Asana.py
+++ Jira2Asana.py
@@ -5,10 +5,11 @@
 import csv
 import glob
 from datetime import datetime
+from collections import Counter
 
 # Set this to a positive number if you want to include only a few rows for testing:
 # TODO: Set this to 0 when running with complete data:
-sampleSize = 10
+sampleSize = 0
 
 def convertJiraCsvToAsanaCsv(jiraCsvFileName):
 	fileNameWithoutExtension = jiraCsvFileName[:-4]
@@ -38,7 +39,7 @@ def convertJiraCsvToAsanaCsv(jiraCsvFileName):
 	asanaCsvFileName = "Asana-" + fileNameWithoutExtension + ".csv"
 	asanaCsvfile = open(asanaCsvFileName, 'w')
 	csvwriter = csv.writer(asanaCsvfile, dialect='excel')
-	csvwriter.writerow(['Name', 'Description', 'Assignee', 'Collaborators', 'Due Date', 'Section / Column', 'Subtask of', 'Status', 'Priority', 'Type', 'Created by', 'Components', 'Labels', 'JiraKey', 'Created on', 'Project'])
+	csvwriter.writerow(['Name', 'Description', 'Assignee', 'Collaborators', 'Due Date', 'Section / Column', 'Subtask of', 'Status', 'Priority', 'Type', 'Created by', 'Components', 'Labels', 'JiraKey', 'Created on', 'Platform'])
 
 	def mapAndWriteRow(row):
 		title = getValue(row, "Summary")
@@ -51,8 +52,11 @@ def convertJiraCsvToAsanaCsv(jiraCsvFileName):
 		assignee = getValue(row, "Assignee")
 		jiraKey = getValue(row, "Issue key")
 		components = ",".join(getValues(row, "Component/s"))
-		labels = ",".join(getValues(row, "Labels"))
 		created = getValue(row, "Created")
+		year = "CreatedIn-" + created[6:10]
+		labels = getValues(row, "Labels")
+		labels.append(year)
+		labelsField = ",".join(labels)
 		project = getValue(row, "Project name")
 
 		createdDate = datetime.strptime(created[:10], '%d.%m.%Y')
@@ -76,13 +80,14 @@ def convertJiraCsvToAsanaCsv(jiraCsvFileName):
 		createdByField = reporter if "@schibsted.com" in reporter else None
 		assigneeField = assignee if "@schibsted.com" in assignee else None
 		section = sprint if sprint else "Backlog"
+		platformField = project
 
 		# TODO: Remove these in final run:
-		createdByField =  "ole.lynge.soerensen@schibsted.com"
-		assigneeField = None
-		collaborators = None
+		#createdByField =  "ole.lynge.soerensen@schibsted.com"
+		#assigneeField = None
+		#collaborators = None
 
-		csvwriter.writerow([title, descriptionField, assigneeField, collaborators, dueDate, section, subtaskOf, status, priority, issueType, createdByField, components, labels, jiraKey, createdField, project])
+		csvwriter.writerow([title, descriptionField, assigneeField, collaborators, dueDate, section, subtaskOf, status, priority, issueType, createdByField, components, labelsField, jiraKey, createdField, platformField])
 
 
 	with open(jiraCsvFileName, newline='') as f:
@@ -100,10 +105,55 @@ def convertJiraCsvToAsanaCsv(jiraCsvFileName):
 				mapAndWriteRow(row)
 			rowIndex += 1
 			if sampleSize > 0 and rowIndex > sampleSize: break
-
+		print("Converted " + str(rowIndex) + " issues from " + jiraCsvFileName)
 
 	asanaCsvfile.close()
 	logFile.close()
 
-convertJiraCsvToAsanaCsv("DBA-BST.csv")
-convertJiraCsvToAsanaCsv("Motors-BST.csv")
+def listUsers(jiraCsvFileName):
+	def getValues(row, fieldName):
+		return [value for index, value in enumerate(row) if value and header[index] == fieldName]
+
+	def getValue(row, fieldName):
+		values = getValues(row, fieldName)
+		return "\n".join(values)
+
+	def appendValue(input, row, fieldName):
+		values = getValues(row, fieldName)
+		if not values: return
+		input.append("\n".join(values))
+
+	def getWatchers(row):
+		return [x for index, x in enumerate(row) if x and header[index] == "Watchers"]
+
+	def getUsers(row):
+		result = []
+		appendValue(result, row, "Assignee")
+		appendValue(result, row, "Reporter")
+		appendValue(result, row, "Creator")
+		return result + getWatchers(row)
+
+	with open(jiraCsvFileName, newline='') as f:
+		reader = csv.reader(f)
+		rowIndex = 0
+		users = []
+		for row in reader:
+			if rowIndex == 0:
+				header = row
+			else:
+				users += getUsers(row)
+			rowIndex += 1
+		print("\n" + jiraCsvFileName + " :")
+		print("Total usages: " + str(len(users)))
+		counts = Counter(users)
+		for user in counts:
+			print(str(counts[user]) + " : " + user)
+		#users = list(set(users))
+		#users.sort()
+		#for user in users:
+		#	print(user)
+
+convertJiraCsvToAsanaCsv("DBA-BST-2022-10-24.csv")
+convertJiraCsvToAsanaCsv("Motors-BST-2022-10-24.csv")
+#listUsers("Jira-DBA-2022.csv")
+#listUsers("Jira-Motors-2022.csv")
